7c74d189c615ac937494d03e4889182eabc2cde2
evilhere7 <evilmc777@gmail.com>
2026-08-20 22:04:44 +0545

    Fix Cloudflare deployment

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // Using default Cloudflare Workers runtime with Node.js compatibility
+});
